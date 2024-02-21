2da70ea92ce4644a872c72c4235ab91f8bb3e28b
flesten-ali <fbada858@gmail.com>
2024-02-21 23:28:10 +0200

    boot

--- HangManGame/endTheGame.js
+++
@@ -1,8 +0,0 @@
-function endTheGame() {
-  // End Div
-  let finishDiv = document.createElement("div");
-  //   End Text
-  let finishText = document.createTextNode(
-    `The Game Over, The Word Is ${randomValueName}`
-  );
-}
