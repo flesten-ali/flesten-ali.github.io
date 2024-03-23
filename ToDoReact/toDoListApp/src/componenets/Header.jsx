c3f84099458db73f0e7d041a6494e8227c758020
flesten-ali <fbada858@gmail.com>
2024-03-24 00:23:28 +0200

    a

--- ToDoReact/toDoListApp/src/componenets/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "../cssModules/header.module.css";
-export default function Header() {
-  return (
-    <div>
-      <h1 className={styles.header}>My ToDo List</h1>
-    </div>
-  );
-}
